4106500a1101eb20168429a5bdddc92ac42e87b2
evinsdesign <kelvinsaputra024@gmail.com>
2024-04-17 17:10:23 +0800

    import assets, fonts, tailwind.config

--- index.php
+++ index.php
@@ -6,7 +6,65 @@
     <title>Lontar Bali</title>
     <link rel="stylesheet" href="./src/output.css">
 </head>
-<body class="flex justify-center">
-     <h1 class="text-red-500">heello</h1>
+<body>
+    <!-- navbar -->
+    <nav
+      class="bg-darkBlue w-ful xxsm:py-2 xxsm:px-5 xsm:py-2 xsm:px-12 sm:py-3 sm:px-16 md:py-3 md:px-16 lg:flex lg:justify-between lg:items-center lg:py-3 lg:px-16"
+    >
+      <!-- logo -->
+      <div
+        class="xxsm:flex xxsm:justify-between xxsm:items-center xsm:flex xsm:justify-between xsm:items-center sm:flex sm:justify-between sm:items-center md:flex md:justify-between md:items-center"
+      >
+        <a href="#"
+          ><img
+            class="w-40 xxsm:w-24 xsm:w-28 md:w-40"
+            src="image/Logo.svg"
+            height="70px"
+            alt="logo-lontar"
+        /></a>
+        <ion-icon
+          class="text-white text-3xl lg:hidden cursor-pointer"
+          name="menu"
+          id="humburgerMenu"
+          onclick="menubar"
+        ></ion-icon>
+        <ion-icon
+          class="text-white text-3xl xxsm:hidden xsm:hidden sm:hidden md:hidden lg:hidden cursor-pointer"
+          name="close"
+          id="closeHumburgerMenu"
+          onclick="close"
+        ></ion-icon>
+      </div>
+
+      <!-- menu navigation -->
+      <ul
+        id="navigation"
+        class="flex gap-5 xxsm:absolute xxsm:z-10 xxsm:flex-col xxsm:gap-5 xxsm:top-14 xxsm:bg-dark-blue xxsm:left-0 xxsm:px-16 xxsm:py-8 xxsm:hidden xxsm:w-full xsm:absolute xsm:z-10 xsm:flex-col xsm:gap-5 xsm:top-14 xsm:bg-dark-blue xsm:left-0 xsm:px-16 xsm:py-8 xsm:hidden xsm:w-full sm:absolute sm:z-10 sm:flex-col sm:gap-5 sm:top-14 sm:bg-dark-blue sm:left-0 sm:px-16 sm:py-8 sm:hidden sm:w-full md:absolute md:z-10 md:flex-col md:gap-5 md:top-14 md:bg-dark-blue md:left-0 md:px-16 md:py-8 md:hidden md:w-full lg:gap-10 lg:flex lg:px-0 lg:py-0 lg:flex-row lg:justify-center lg:items-center lg:static lg:z-0 text-white lg:bg-transparent lg:w-auto lg:opacity-100 lg:text-lg"
+      >
+        <li class="font-medium">
+          <a
+            href="index.html"
+            class="relative active:text-orange-pastel border-white duration-500 hover:text-orange-pastel"
+            >Beranda</a
+          >
+        </li>
+        <li class="font-medium">
+          <a
+            href="Sejarah-Lontar.html"
+            class="hover:text-orange-pastel duration-500"
+            >Sejarah Lontar</a
+          >
+        </li>
+        <li class="font-medium">
+          <a href="#" class="hover:text-orange-pastel duration-500">Koleksi</a>
+        </li>
+        <li class="font-medium">
+          <a href="#" class="hover:text-orange-pastel duration-500"
+            >Tentang Kami</a
+          >
+        </li>
+      </ul>
+    </nav>
+    <!-- end navbar -->
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,45 @@
 module.exports = {
   content: ["./**/*.{html,js,php}"],
   theme: {
-    extend: {},
+    screens: {
+      xxsm: "320px",
+      // => @media (min-width: 320px) { ... }
+      xsm: "370px",
+      // => @media (min-width: 370px) { ... }
+      base: "480px",
+      // => @media (min-width: 370px) { ... }
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+      "2xl": "1536px",
+      // => @media (min-width: 1280px) { ... }
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      darkBlue: "#2D3250",
+      mediumBlue: "#424769",
+      lightBlue: "#676F9D",
+      orangePastel: "#F9B17A",
+      darkSecondary: "#9A9A9A",
+      lightSecondary: "#D9D9D9",
+      white: "#ffffff",
+      black: "#000000",
+    },
+    extend: {
+      fontFamily: {
+        montsBold: "Montserrat-Bold, sans-serif",
+        montsSemiBold: "Montserrat-SemiBold, sans-serif",
+        montsMedium: "Montserrat-Medium, sans-serif",
+        montsItalic: "Montserrat-Italic, sans-serif",
+        montserrat: "Montserrat-Regular, sans-serif", // Adds a new `font-display` class
+      },
+    },
   },
   plugins: [],
 };
