a49d25287a42f1121e39027579011fc423d6ffd0
evinsdesign <kelvinsaputra024@gmail.com>
2024-04-20 19:42:24 +0800

    feat: Routing with mvc

--- apps/core/App.php
+++ apps/core/App.php
@@ -2,8 +2,20 @@
 
 class App
 {
+    // routing
     public function __construct()
     {
-        echo "OK!";
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+    public function parseURL()
+    {
+        // mengambil url
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
