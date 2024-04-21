a2389fc8b984547b485702e1675f8db5955b44bc
evinsdesign <kelvinsaputra024@gmail.com>
2024-04-21 20:43:02 +0800

    feat: Koleksi Lontar & pagination

---
+++ apps/views/components/user/CardKoleksi.php
@@ -0,0 +1,23 @@
+<div class="flex justify-center items-center mt-5 ">
+    <div class="flex xxsm:flex-col md:flex-row items-center rounded-lg xxsm:w-[250px] xsm:w-[280px] base:w-[360px] sm:w-[560px] md:w-[650px] lg:w-[800px] xl:w-[900px] 2xl:w-[1000px] max-h-full shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] xxsm:flex">
+        <div class="flex flex-col justify-between p-4 leading-normal xxsm:order-2 md:order-none md:text-sm lg:text-base">
+            <h2 class="xl:card-title text-darkBlue font-montsBold xxsm:text-center xxsm:text-lg base:text-xl xl:text-2xl">
+                Bhasma
+            </h2>
+            <h2 class="font-montsMedium text-mediumBlue xl:text-lg">
+                Pedanda Anyar | 22/05/1962 | Belah Batuh, Gianyar
+            </h2>
+            <p class="font-montserrat text-justify xl:text-md">
+                Detail Deskripsi Lengkap Lontar, judul lontar : Bhasma, tipe bahan:
+                Daun Ronta, subjek: Mantra, klasifikasi: Weda,
+                bahasa: Bahasa Kawi, <a href="#" class="text-orangePastel">Selengkapnya</a>
+            </p>
+
+        </div>
+        <figure class="xxsm:order-1 md:order-none object-cover">
+            <img class="bg-cover xxsm:max-w-full lg:max-w-full md:w-[800px] md:h-[200px] lg:h-full  xxsm:rounded-t-lg md:rounded-t-none md:rounded-tr-lg md:rounded-br-lg lg:rounded-r-lg" src="../../../../public/assets/Tulis_Lontar.jpg" alt="image">
+        </figure>
+
+
+    </div>
+</div>
