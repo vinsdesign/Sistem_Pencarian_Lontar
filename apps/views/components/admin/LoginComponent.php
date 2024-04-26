e70e2b8433ba4644f6025d4c617e1799072fed18
evinsdesign <kelvinsaputra024@gmail.com>
2024-04-26 17:33:32 +0800

    feat: login access

--- apps/views/components/admin/LoginComponent.php
+++ apps/views/components/admin/LoginComponent.php
@@ -1,5 +1,44 @@
+<?php
+require './apps/config/config.php';
+if (isset($_POST["login"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+
+    $result = mysqli_query($koneksi, "SELECT * FROM admin WHERE username = '$username'");
+    // cek username
+    if (mysqli_num_rows($result) === 1) { //mysqli_num_rows => ada berapa baris yang dikembalikan
+
+        // check password
+        $row = mysqli_fetch_assoc($result);
+        header("location: /DashboardAdmin");
+        exit;
+    }
+    $error = true;
+}
+?>
 <section class="bg-[url('../public/assets/bg_login.jpg')] w-full h-[100vh]">
+    <div class="flex justify-center items-center">
+        <?php if (isset($error)) : ?>
+            <div id="toast-undo" class="flex justify-center items-center absolute top-0 transition-all  delay-500 w-full max-w-sm p-4 text-gray-500 bg-danger rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 mt-5" role="alert">
+                <div class="text-sm font-normal flex justify-center items-center gap-3">
+                    <i class="fa-solid fa-exclamation text-white text-lg "></i>
+                    <h1 class="text-white font-montsMedium">Username / Password Salah</h1>
+                </div>
+                <div class="flex items-center ms-auto space-x-2 rtl:space-x-reverse">
+                    <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-transparent text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-undo" aria-label="Close">
+                        <span class="sr-only">Close</span>
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
+
     <div class="flex justify-end items-center">
+
         <!-- logo sidebar -->
         <div class="mt-20 mr-20">
             <a href="#" class="flex justify-end -mr-3">
@@ -11,14 +50,14 @@
             <div class="mt-32">
                 <h1 class="text-right font-montsBold text-white text-4xl">LOGIN</h1>
                 <p class="text-right text-base text-orangePastel">Silahkan masukan Username & password. </p>
-                <form class="max-w-md mx-auto">
+                <form class="max-w-md mx-auto" action="" method="post">
                     <div class="relative z-0 w-full mb-2 group">
-                        <input type="email" name="floating_email" id="floating_email" class="block py-5 pb-2 mt-4 w-96 h-16 text-base text-gray-200 bg-mediumBlue border-0 border-l-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:text-white focus:ring-0 focus:border-orangePastel peer" placeholder=" " required />
-                        <label for="floating_email" class="peer-focus:font-medium px-5 py-2 absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-5 scale-75 top-4 z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-orangePastel peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3">Email address</label>
+                        <input type="text" name="username" id="username" class="block py-5 pb-2 mt-4 w-96 h-16 text-sm text-gray-200 bg-mediumBlue border-0 border-l-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:text-white focus:ring-0 focus:border-orangePastel peer" placeholder=" " required />
+                        <label for="username" class="peer-focus:font-medium px-5 py-2 absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-5 scale-75 top-4 z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-orangePastel peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3">Username</label>
                     </div>
                     <div class="relative z-0 w-full mb-3 group">
-                        <input type="password" name="floating_pass" id="floating_pass" class="block py-5 pb-2 mt-4 w-96 h-16 text-base text-gray-200 bg-mediumBlue border-0 border-l-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:text-white focus:ring-0 focus:border-orangePastel peer" placeholder=" " required />
-                        <label for="floating_pass" class="peer-focus:font-medium px-5 py-2 absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-5 scale-75 top-4 z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-orangePastel peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3">Password</label>
+                        <input type="password" name="password" id="password" class="block py-5 pb-2 mt-4 w-96 h-16 text-base text-gray-200 bg-mediumBlue border-0 border-l-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:text-white focus:ring-0 focus:border-orangePastel peer" placeholder=" " required />
+                        <label for="password" class="peer-focus:font-medium px-5 py-2 absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-5 scale-75 top-4 z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-orangePastel peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3">Password</label>
                     </div>
                     <div class="flex justify-end items-center mb-5">
                         <div class="flex items-center h-5">
@@ -27,7 +66,7 @@
                         <label for="remember-me" class="ms-2 text-base font-montsMedium text-white dark:text-gray-300">Remember me</label>
                     </div>
                     <div class="flex justify-end">
-                        <button type="submit" class="text-mediumBlue font-montsSemiBold bg-orangePastel hover:bg-orange-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        <button type="submit" class="text-mediumBlue font-montsSemiBold bg-orangePastel hover:bg-orange-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" name="login">Submit</button>
                     </div>
 
                 </form>
